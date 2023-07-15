a090558f4e1c2ca9e6b72c86d96729ed09191f8c
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2023-07-15 11:29:02 +0100

    [Feature] Enhance SEO and optimize desktop responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ function App() {
   return (
     <>
       <main className="flex w-full justify-center items-center h-screen bg-blue-100 p-6">
-        <div className="card__wrapper max-w-md bg-white rounded-[1.2rem] p-4">
-          <div className="flex flex-col qr__code__wrapper">
+        <div className=" max-w-md bg-white rounded-[1.2rem] p-4">
+          <div className="flex flex-col">
             <img
               src="/images/image-qr-code.png"
               alt="qr code image"
               className="w-100 h-auto rounded-2xl"
             />
           </div>
-          <h1 className="text-[1.4rem] text-center p-4 font-bold text-slate-900">
+          <h1 className="text-[1.4rem]  text-center p-4 font-bold text-slate-900 md:text-[1.8rem]">
             Improve your front-end skills by building projects
           </h1>
-          <p className="text-normal text-center text-slate-600 p-4 pb-6">
+          <p className="text-normal text-center text-slate-600 p-4 pb-4 md:text-[1.2rem] ">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
